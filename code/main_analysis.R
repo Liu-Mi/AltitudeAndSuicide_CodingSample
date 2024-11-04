ae5bc907fd8455ea5e1e38f0643f8a6f90564b96
Mi Liu <mi.liu9906@gmail.com>
2024-11-04 16:50:51 +0100

    Added extraction of mediator effect size to main analysis

--- code/main_analysis.R
+++ code/main_analysis.R
@@ -12,6 +12,7 @@ library(sf)
 library(haven)
 library(stargazer)
 library(xtable)
+library(tidycensus)
 select <- dplyr::select
 
 # Clean data, visualise and load functions
@@ -54,7 +55,7 @@ brenner_lm_other <- lm(
     data = suicide_data_fe
 )
 
-brenner_lms <- list(brenner_lm_overall, brenner_lm_firearms, brenner_lm_other)
+brenner_lms <- list(overall = brenner_lm_overall, firearms = brenner_lm_firearms, other = brenner_lm_other)
 
 # Calculate brackets statistics and marginal effect of moving up an altitude bracket
 altitude_brackets_stats_df <- calculate_altitude_brackets_stats(suicide_data, suicide_data_fe, step_size, no_alt_steps, brenner_lms)
@@ -64,17 +65,6 @@ altitude_brackets_stats_latex <- xtable(altitude_brackets_stats_df, caption = "A
 print(altitude_brackets_stats_latex,
     type = "latex", file = "tables/altitude_brackets_stats.tex",
     include.rownames = FALSE, size = "\\footnotesize", sanitize.text.function = function(x) x
-)
-
-# Create regression output table
-stargazer(
-    ... = brenner_lms, out = "tables/brenner.tex", label = "tab:brenner", title = "Brenner et al. replication",
-    dep.var.labels = c("Overall suicides", "Firarm suicides", "Nonfirearm suicides"),
-    font.size = "scriptsize", single.row = TRUE,
-    covariate.labels = c(
-        altitude_brackets_text, "Fifty and above", "Sex Ratio", "Percent White",
-        "Median household income (level)", "Population density (level)"
-    )
 )
 
 
@@ -92,6 +82,69 @@ main_model <- main_regression_models(suicide_data, c("state", "gun_dealer_licens
 
 # Generate LaTeX tables and plots for the main model
 generate_latex_and_plots(main_model, "main_model", "Regression Results of Main Model", ylim_range = c(-0.005, 0.005), omit = c("altitude", "state"))
+
+
+### Decompose size of mediator effects
+# Get all relevant mediator variables
+mediator_data <- suicide_data %>%
+    select(-c(
+        county, county_code, state, population,
+        fifty_and_above, sex_ratio, percent_white, log_median_household_income, log_population_density,
+        ends_with("rate")
+    ), gun_dealer_licenses_rate, suicide_overall_rate) %>%
+    mutate(across(c(everything(), -c(altitude, suicide_overall_rate)), scale))
+level_terms <- mediator_data %>%
+    select(-c(altitude, suicide_overall_rate)) %>%
+    names()
+
+# Check how well altitude explains each variable
+lm_alt_med <- level_terms %>% lapply(function(x, data) lm(as.formula(paste(x, "~ altitude")), mediator_data, weights = mediator_data$weights))
+coeffs_alt_med <- lm_alt_med %>%
+    lapply(function(x) data.frame(coefficients = coefficients(x)[2], p_values = summary(x)$coefficients[2, 4])) %>%
+    do.call(what = rbind)
+row.names(coeffs_alt_med) <- level_terms
+coeffs_alt_med %>% filter(p_values > 0.05) # Lists variables that aren't explained by altitude
+
+# Check how well each variable explains suicide rates
+lm_suicide_med <- level_terms %>% lapply(function(x, data) lm(as.formula(paste("suicide_overall_rate ~", x)), mediator_data, weights = mediator_data$weights))
+coeffs_suicide_med <- lm_suicide_med %>%
+    lapply(function(x) data.frame(coefficients = coefficients(x)[2], p_values = summary(x)$coefficients[2, 4])) %>%
+    do.call(what = rbind)
+coeffs_suicide_med %>% filter(p_values > 0.05) # List variables that don't explain suicide rates
+coeffs_suicide_med %>% arrange(desc(abs(coefficients)))
+
+# Check how well each variable explains suicide rates conditional on altitude
+lm_cond_suicide_med <- level_terms %>% lapply(function(x, data) lm(as.formula(paste("suicide_overall_rate ~", x, " + altitude")), mediator_data, weights = mediator_data$weights))
+coeffs_cond_suicide_med <- lm_cond_suicide_med %>%
+    lapply(function(x) data.frame(coefficients = coefficients(x)[2], p_values = summary(x)$coefficients[2, 4])) %>%
+    do.call(what = rbind)
+coeffs_cond_suicide_med %>% arrange(desc(abs(coefficients)))
+
+# Order size of mediator effect according to relationship with suicide conditional on altitude
+order_mediators <- coeffs_cond_suicide_med %>%
+    filter(p_values <= 0.05) %>%
+    arrange(desc(abs(coefficients))) %>%
+    row.names()
+order_mediators <- c("state", order_mediators)
+
+# Add variables one by one to regression
+mediator_names <- c("altitude", c(order_mediators) %>% Reduce(f = c, accumulate = TRUE))
+mediator_effect_lms <- mediator_names %>% lapply(function(x) {
+    data <- suicide_data %>% select(-c(ends_with("rate"), all_of(order_mediators %>% setdiff(x))), gun_dealer_licenses_rate, starts_with("suicide"))
+    main_regression_models(data, x)
+})
+
+# Extract size of mediator effects
+mediator_effects <- mediator_effect_lms %>%
+    lapply(function(x) {
+        coefficients <- coefficients(x$overall)
+        coefficient <- coefficients[names(coefficients) == "altitude"]
+        coefficient
+    }) %>%
+    unlist() %>%
+    diff()
+names(mediator_effects) <- order_mediators
+mediator_effects
 
 
 ### Simple Robustness checks
@@ -113,7 +166,6 @@ alt_fixed_effects <- main_regression_models(suicide_data_fe, c("state", "gun_dea
 
 # Create statistics as with Brenner et al.
 alt_fixed_effects_df <- altitude_brackets_stats_df[, -c((length(altitude_brackets_stats_df) - 2):length(altitude_brackets_stats_df))]
-
 alt_fixed_effects_df <- add_marginal_effects(alt_fixed_effects_df, alt_fixed_effects)
 
 # Create LaTeX table with mean altitude
@@ -122,82 +174,10 @@ print(alt_fixed_effects_df_latex,
     type = "latex", file = "tables/alt_fixed_effects_me.tex",
     include.rownames = FALSE, size = "\\footnotesize", sanitize.text.function = function(x) x
 )
+
+# Create regression output table
 stargazer(
     ... = alt_fixed_effects, out = "tables/alt_fixed_effects.tex", label = "tab:alt_fixed_effects", title = "Atltiude fixed effects model",
     dep.var.labels = c("Overall suicides", "Firarm suicides", "Nonfirearm suicides"),
     font.size = "scriptsize", single.row = TRUE, omit = c("state")
-)
-
-
-### Model with stepwise altitude fixed effects
-# Add stepwise altitude fixed effects
-alt_fe_stepwise_data <- suicide_data %>%
-    mutate(altitude = pmin(pmax(floor(altitude / step_size), 0), no_alt_steps)) %>%
-    filter(!is.na(altitude))
-alt_fe_matrix <- alt_fe_stepwise_data$altitude %>%
-    lapply(FUN = function(x) {
-        c(rep(1, times = x), rep(0, no_alt_steps - x))
-    }) %>%
-    unlist() %>%
-    matrix(ncol = no_alt_steps, byrow = TRUE)
-colnames(alt_fe_matrix) <- paste0("altitude_fe", 1:(no_alt_steps))
-
-dod_data_altitude_fe <- cbind(alt_fe_stepwise_data, alt_fe_matrix)
-
-# Run model with altitude fixed effects
-dod_data_altitude_fe_lm <- main_regression_models(dod_data_altitude_fe, c("state", "gun_dealer_licenses_rate"), c(interaction_terms), "altitude")
-
-# Run hypothesis tests for altitude up to a bracket
-types <- c("overall", "firearms", "other")
-hypotheses_list <- lapply(types, function(type) {
-    p_values <- numeric(no_alt_steps)
-    for (x in 1:no_alt_steps) {
-        hypothesis_test <- hypotheses(dod_data_altitude_fe_lm[[which(types == type)]], paste0(paste0("altitude_fe", 1:x, collapse = "+"), " = 0"))
-        p_values[x] <- hypothesis_test$p.value
-    }
-    p_values
-})
-names(hypotheses_list) <- types
-
-# Create a dataframe with the coefficients of the different types
-coefficients_df <- data.frame(
-    altitude_bracket = 1:no_alt_steps,
-    overall = extract_coefficients(dod_data_altitude_fe_lm[[1]], "altitude"),
-    firearms = extract_coefficients(dod_data_altitude_fe_lm[[2]], "altitude"),
-    other = extract_coefficients(dod_data_altitude_fe_lm[[3]], "altitude")
-)
-
-# Add hypothesis results to the dataframe
-coefficients_df <- coefficients_df %>%
-    mutate(
-        overall_significant = hypotheses_list$overall < 0.05,
-        firearms_significant = hypotheses_list$firearms < 0.05,
-        other_significant = hypotheses_list$other < 0.05
-    )
-
-# Pivot the coefficients dataframe to a longer format for better visualization
-coefficients_long_df <- coefficients_df %>%
-    pivot_longer(cols = c(overall, firearms, other), names_to = "type", values_to = "estimate") %>%
-    pivot_longer(cols = c(overall_significant, firearms_significant, other_significant), names_to = "significance_type", values_to = "significant") %>%
-    filter(type == sub("_significant", "", significance_type)) %>%
-    mutate(
-        estimate = ifelse(significant, paste0("$\\pmb{", round(estimate, 3), "}$"), round(estimate, 3))
-    ) %>%
-    select(altitude_bracket, type, estimate) %>%
-    pivot_wider(names_from = type, values_from = estimate)
-coefficients_long_df$altitude_bracket <- paste0("Altitude $\\geq$ ", seq(step_size, (no_alt_steps) * step_size, step_size), "m")
-colnames(coefficients_long_df) <- c("Bracket", "Overall", "Firearms", "Other")
-
-# Create LaTeX table with the coefficients
-coefficients_long_df_latex <- xtable(coefficients_long_df, caption = "Coefficients of the stepwise altitude fixed effects model", label = "tab:alt_fe_stepwise_coefficients")
-print(coefficients_long_df_latex,
-    type = "latex", file = "tables/alt_fe_stepwise_coefficients.tex",
-    include.rownames = FALSE, size = "\\footnotesize", sanitize.text.function = function(x) x
-)
-
-# stargarzer table
-stargazer(
-    ... = dod_data_altitude_fe_lm, out = "tables/alt_fe_stepwise.tex", label = "tab:alt_fe_stepwise", title = "Stepwise altitude fixed effects model",
-    dep.var.labels = c("Overall suicides", "Firarm suicides", "Nonfirearm suicides"),
-    font.size = "scriptsize", single.row = TRUE, omit = c("altitude", "state")
 )
